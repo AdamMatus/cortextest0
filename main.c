d5bd2c66fc6d4c73f35dc998360049c8d5794805
AdamMatus <adamkud@gmail.com>
2016-05-18 22:50:49 +0200

    time printing on serial LOGS

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stm32f4xx.h>
+#include <stdio.h>
+#include <core_cm4.h>
+
+const uint16_t LOG_TIME_SIZE_FORMAT = 7;
 
 void TIM1_UP_TIM10_IRQHandler();
 void put_log_mesg(char* mesg);
+volatile uint32_t systemTicks;
 
 unsigned int onoffleds;
 volatile uint32_t LOG_LED_FLAG;
 char recived_char;
 
+
+
 int main()
 {
 	//SysClock - HSI 16 MHz
+	//SysTickConfig
+	SysTick->LOAD = 16000000/(8*1000)-1;	
+	SysTick->VAL = 0;
+	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk + SysTick_CTRL_ENABLE_Msk;
+
 	//AHB = APB1 = APB2
 	//GPIOG clock enabling, USART2 clock enabling
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
@@ -45,9 +57,11 @@ int main()
 	//
 	
 	USART2 ->CR1 |=  USART_CR1_TCIE + USART_CR1_RXNEIE +  USART_CR1_RE;
+
+
 	while(1){
 		if(LOG_LED_FLAG & 0x01){
-			put_log_mesg("LEDS just TOOGLED! xD");
+			put_log_mesg("LEDS just TOOGLED! xD\n");
 			LOG_LED_FLAG &= ~0x01;
 		}
 	}
@@ -84,8 +98,40 @@ void USART2_IRQHandler()
 	}
 }	
 
+void SysTick_Handler()
+{
+	systemTicks++;
+}
+
 void put_log_mesg(char* mesg){
-	USART2 -> CR1 |= USART_CR1_TE ;
+	//put clock log
+	char data_string[LOG_TIME_SIZE_FORMAT + 1]; // additional sign ' '
+	uint32_t sys_tick_copy = systemTicks/1000; //seconds asumming that stick is 1ms
+
+	uint32_t index = 0;
+	data_string[index++] = ' '; // separator at the end of the time ( xD )
+
+	uint32_t modulosixty;
+	for(uint32_t i = 0; i<2; i++){
+		modulosixty = sys_tick_copy%60;
+		data_string[index++] = '0' + modulosixty%10;
+  	modulosixty/=10;
+  	data_string[index++] = '0' + modulosixty;
+		sys_tick_copy/=60; //minutes after i=0 and hours after i=1
+		data_string[index++] = ':';
+	}
+	
+	for(uint32_t i = 0; i < LOG_TIME_SIZE_FORMAT - 6; i++){ // 6 - six signes generated yet
+		data_string[index++] = '0' + sys_tick_copy%10;
+		sys_tick_copy/=10;
+	}	
+
+ 	USART2 -> CR1 |= USART_CR1_TE ;
+
+	while(index--){ // print backfoward, fuck script kiddies im the hacker
+		while( !(USART2->SR & USART_SR_TXE) );
+		USART2 -> DR = data_string[index];
+	}
 
 	while(*mesg){
 		while( !(USART2->SR & USART_SR_TXE) );
